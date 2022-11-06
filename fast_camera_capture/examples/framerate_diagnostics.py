498a82239453c468ed05d388b53de1241f21f5d6
Jon <jonmatthis@gmai.com>
2022-11-06 16:39:50 -0500

    looks good

--- fast_camera_capture/examples/framerate_diagnostics.py
+++ fast_camera_capture/examples/framerate_diagnostics.py
@@ -1,57 +1,82 @@
 import time
+from dataclasses import dataclass
 
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib
 from scipy.stats import median_abs_deviation
 from rich import print
 
-matplotlib.use("qt5agg")
-matplotlib.set_loglevel("warning")
+from fast_camera_capture.detection.detect_cameras import detect_cameras
 
 from fast_camera_capture.opencv.group.camera_group import CameraGroup
 
+@dataclass
+class TimestampDiagnosticsDataClass:
+    mean_framerates_per_camera: dict
+    standard_deviation_framerates_per_camera: dict
+    median_framerates_per_camera: dict
+    median_absolute_deviation_per_camera: dict
+    mean_mean_framerate: float
+    mean_standard_deviation_framerates: float
+    mean_median_framerates: float
+    mean_median_absolute_deviation_per_camera: float
 
-def show_timestamp_diagnostic_plots(timestamps_dictionary: dict, shared_zero_time: int):
-    # plot timestamps
+
+def show_timestamp_diagnostic_plots(timestamps_dictionary: dict, shared_zero_time: int, pause_on_show: bool = False):
+    import matplotlib.pyplot as plt
+    import matplotlib
+    matplotlib.use("qt5agg")
+    matplotlib.set_loglevel("warning")
+    plt.ion()
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(10, 10))
-    max_frame_duration = .1 #sec
 
-    ax2.plot([0, 100], [.033, .033], 'k-', label="30 fps")
-    ax3.plot([.033, .033], [0, 100], 'k-', label="30 fps")
+    # plot timestamps
+    max_frame_duration = .1  # sec
+
+    ax1.set_xlabel("Frame number")
+    ax1.set_ylabel("Timestamp (sec)")
+    ax1.set_title("Timestamps from shared zero")
+
+    ax2.set_ylim(0, max_frame_duration)
+    ax2.set_xlabel("Frame number")
+    ax2.set_ylabel("Frame duration (sec)")
+    ax2.set_title("Frame Duration (time elapsed between timestamps)")
+
+    ax3.set_xlabel("Frame duration (sec)")
+    ax3.set_ylabel("Number of frames with a given duration")
+    ax3.set_title("Histogram of frame durations")
+
+    number_of_frames = len(list(timestamps_dictionary.values())[0])
+    ax2.plot([0, number_of_frames], [.033, .033], 'k-', label="30 fps")
+    ax3.plot([.033, .033], [0, number_of_frames], 'k-', label="30 fps")
 
     for cam_id, timestamps in timestamps_dictionary.items():
         timestamps_formatted = np.asarray(timestamps) - shared_zero_time
         timestamps_formatted = timestamps_formatted / 1e9
 
-        ax1.plot(timestamps_formatted,'.-', label=cam_id)
-        ax1.set_xlabel("Frame number")
-        ax1.set_ylabel("Timestamp (sec)")
-        ax1.set_title("Timestamps from shared zero")
+
+        ax1.plot(timestamps_formatted, '.-', label=cam_id)
         ax1.legend()
 
         ax2.plot(np.diff(timestamps_formatted), '.', label=cam_id, alpha=.5)
-        ax2.set_ylim(0, max_frame_duration)
-        ax2.set_xlabel("Frame number")
-        ax2.set_ylabel("Frame duration (sec)")
-        ax2.set_title("Frame Duration (time elapsed between timestamps)")
+
         ax2.legend()
+
 
         ax3.hist(np.diff(timestamps_formatted),
                  bins=np.arange(0, max_frame_duration, .001),
                  label=cam_id,
                  alpha=0.5,
                  density=True)
-
-        ax3.set_xlabel("Frame duration (sec)")
-        ax3.set_ylabel("Number of frames with a given duration")
-        ax3.set_title("Histogram of frame durations")
         ax3.legend()
 
     plt.show()
+    plt.pause(.1)
+    if pause_on_show:
+        input("Press Enter to continue...")
+        plt.close()
 
 
-def calculate_camera_diagnostic_results(timestamps_dictionary)->dict:
+def calculate_camera_diagnostic_results(timestamps_dictionary) -> TimestampDiagnosticsDataClass:
     mean_framerates_per_camera = {}
     standard_deviation_framerates_per_camera = {}
     median_framerates_per_camera = {}
@@ -70,21 +95,19 @@ def calculate_camera_diagnostic_results(timestamps_dictionary)->dict:
     mean_median_framerates = np.nanmean(list(median_framerates_per_camera.values()))
     mean_median_absolute_deviation_per_camera = np.nanmean(list(median_absolute_deviation_per_camera.values()))
 
-    return {"mean_framerates_per_camera": mean_framerates_per_camera,
-            "standard_deviation_framerates_per_camera": standard_deviation_framerates_per_camera,
-            "median_framerates_per_camera": median_framerates_per_camera,
-            "median_absolute_deviation_per_camera": median_absolute_deviation_per_camera,
-            "mean_mean_framerate": mean_mean_framerate,
-            "mean_standard_deviation_framerates": mean_standard_deviation_framerates,
-            "mean_median_framerates": mean_median_framerates,
-            "mean_median_absolute_deviation_per_camera": mean_median_absolute_deviation_per_camera}
-
-
+    return TimestampDiagnosticsDataClass(mean_framerates_per_camera=mean_framerates_per_camera,
+                                         standard_deviation_framerates_per_camera=standard_deviation_framerates_per_camera,
+                                         median_framerates_per_camera=median_framerates_per_camera,
+                                         median_absolute_deviation_per_camera=median_absolute_deviation_per_camera,
+                                         mean_mean_framerate=float(mean_mean_framerate),
+                                         mean_standard_deviation_framerates=float(mean_standard_deviation_framerates),
+                                         mean_median_framerates=float(mean_median_framerates),
+                                         mean_median_absolute_deviation_per_camera=float(mean_median_absolute_deviation_per_camera))
 
 
 if __name__ == "__main__":
-    cam_ids = ["0", "1", "2", "3", "4", "5", "6"]
-    # cam_ids = ["0"]
+    found_camera_response = detect_cameras()
+    cam_ids = found_camera_response.cameras_found_list
     g = CameraGroup(cam_ids)
     g.start()
 
@@ -94,7 +117,8 @@ if __name__ == "__main__":
 
     break_after_n_frames = 200
     shared_zero_time = time.perf_counter_ns()
-    while True:
+    should_continue = True
+    while should_continue:
         prev_loop_time = loop_time
         loop_time = time.perf_counter_ns()
         loop_duration = (loop_time - prev_loop_time) / 1e6
@@ -104,13 +128,13 @@ if __name__ == "__main__":
             if frame_payload is not None:
                 if frame_payload.success:
                     timestamps_dictionary[cam_id].append(frame_payload.timestamp_ns)
+            if len(timestamps_dictionary[cam_id]) > break_after_n_frames:
+                should_continue = False
 
         print(
             f"Loop duration: {loop_duration:.3f} ms: Timestamps: {[len(val) for val in timestamps_dictionary.values()]}")
 
-        if len(timestamps_dictionary[cam_id]) > break_after_n_frames:
-            break
+    timestamp_diagnostic_data_class = calculate_camera_diagnostic_results(timestamps_dictionary)
+    print(timestamp_diagnostic_data_class.__dict__)
+    show_timestamp_diagnostic_plots(timestamps_dictionary, shared_zero_time, pause_on_show=True)
 
-    timestamp_diagnostic_results_dict = calculate_camera_diagnostic_results(timestamps_dictionary)
-    print(timestamp_diagnostic_results_dict)
-    show_timestamp_diagnostic_plots(timestamps_dictionary, shared_zero_time)
