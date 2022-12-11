537e79b88ec0ed361fef8a5bd287d5ab053dfa27
jonmatthis <jonmatthis@gmail.com>
2022-12-11 10:12:34 -0500

    hard re-factor of aaron's qt viewer

--- fast_camera_capture/__init__.py
+++ fast_camera_capture/__init__.py
@@ -1,6 +1,8 @@
 from fast_camera_capture.opencv.camera.camera import Camera
 from fast_camera_capture.opencv.camera.models.camera_id import WebcamConfig
-from fast_camera_capture.synchronized_video_recorder.synchronized_video_recorder import SynchronizedVideoRecorder
+from fast_camera_capture.synchronized_video_recorder.synchronized_video_recorder import (
+    SynchronizedVideoRecorder,
+)
 from fast_camera_capture.system.log_config.logsetup import configure_logging
 
 
--- fast_camera_capture/opencv/video_recorder/save_synchronized_videos.py
+++ fast_camera_capture/opencv/video_recorder/save_synchronized_videos.py
@@ -2,7 +2,7 @@ from fast_camera_capture import SynchronizedVideoRecorder
 
 if __name__ == "__main__":
     """
-    Connects to every available (opencv compatible) USB camera ans  
+    Connects to every available (opencv compatible) USB camera ans
     records synchronized videos (as `.mp4` files) to a folder.
     Also creates diagnostic plots to ensure synchronization and a `session_information.json` file.
     """
