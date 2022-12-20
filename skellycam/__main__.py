e481c0ffa896eb60f62da15d89af6579f3586948
jonmatthis <jonmatthis@gmail.com>
2022-12-20 12:41:31 -0500

    cleanup

--- skellycam/__main__.py
+++ skellycam/__main__.py
@@ -1,8 +1,11 @@
 # __main__.py
 from skellycam.qt_gui.qt_gui_main import qt_gui_main
 
+def main():
+    qt_gui_main()
+
 if __name__ == '__main__':
     print("This is printing from `skellycam/__main__.py`")
-    qt_gui_main()
+    main()
 
 
