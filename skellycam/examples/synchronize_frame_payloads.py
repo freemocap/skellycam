276a73f4a6401686bda664fb21da10191db44a4f
mprib <prible@gmail.com>
2022-12-29 15:17:40 -0600

    apply formatting

--- skellycam/examples/synchronize_frame_payloads.py
+++ skellycam/examples/synchronize_frame_payloads.py
@@ -1,4 +1,3 @@
-
 import asyncio
 import logging
 import multiprocessing
@@ -24,22 +23,19 @@ async def show_synched_frames(camera_ids_list: list = None):
     camera_group.start()
     should_continue = True
 
-
+    # will be receiving a bundle of synched frames via queue
     syncr = Synchronizer(camera_ids_list)
-
     bundle_q = Queue()
     syncr.subscribe_to_bundle(bundle_q)
-     
+
+    # bundle data and frame times are just for summary reporting...saved to csv in default session folder path
     bundle_index = 0
-    bundle_data = {"Bundle":[]}
+    bundle_data = {"Bundle": []}
 
     for port in camera_ids_list:
         bundle_data[f"Port_{port}_Time"] = []
 
-    frame_times = {"port": [],
-                   "frame_index": [],
-                   "frame_time": [] }
-
+    frame_times = {"port": [], "frame_index": [], "frame_time": []}
 
     for p in multiprocessing.active_children():
         print(f"before big frame loop - found child process: {p}")
@@ -51,52 +47,65 @@ async def show_synched_frames(camera_ids_list: list = None):
         latest_frame_payloads = camera_group.latest_frames()
         for cam_id, frame_payload in latest_frame_payloads.items():
             if frame_payload is not None:
-                payload_count+=1
-                
-    print("------------------------------------------------------------------------------")
+                payload_count += 1
+
+    print(
+        "------------------------------------------------------------------------------"
+    )
 
     while should_continue:
         latest_frame_payloads = camera_group.latest_frames()
-        
+
         for cam_id, frame_payload in latest_frame_payloads.items():
             if frame_payload is not None:
-                syncr.add_frame_payload(frame_payload)
-                frame_times["port"].append(frame_payload.camera_id)        
+                syncr.add_frame_payload(
+                    frame_payload
+                )  # the line that does something; give synchronizer a payload
+
+                # dictionary below is just to create summary output
+                frame_times["port"].append(frame_payload.camera_id)
                 frame_times["frame_index"].append(frame_payload.frame_number)
                 frame_times["frame_time"].append(frame_payload.timestamp_ns)
 
         if not bundle_q.empty():
+            # when a synched bundle is available, the synchronizer pushes it out to subscribers
+
             new_bundle = bundle_q.get()
 
             bundle_data["Bundle"].append(bundle_index)
             bundle_index += 1
 
+            # the block below is just for display and summary output
             for port, frame_data in new_bundle.items():
                 if frame_data:
                     cv2.imshow(f"Port {port}", frame_data["frame"])
                     bundle_data[f"Port_{port}_Time"].append(frame_data["frame_time"])
                 else:
-                    bundle_data[f"Port_{port}_Time"].append("dropped")     
+                    bundle_data[f"Port_{port}_Time"].append("dropped")
 
         if cv2.waitKey(1) == 27:
             logger.info(f"ESC key pressed - shutting down")
             cv2.destroyAllWindows()
             should_continue = False
 
-
     camera_group.close()
 
+    # export summary output
     frame_times = pd.DataFrame(frame_times)
-    frame_times.to_csv(Path(default_session_folder_path(create_folder=True),"frame_times.csv"))
+    frame_times.to_csv(
+        Path(default_session_folder_path(create_folder=True), "frame_times.csv")
+    )
 
     bundle_data = pd.DataFrame(bundle_data)
-    bundle_data.to_csv(Path(default_session_folder_path(create_folder=True),"bundle_data.csv"))
-    
+    bundle_data.to_csv(
+        Path(default_session_folder_path(create_folder=True), "bundle_data.csv")
+    )
+
 
 if __name__ == "__main__":
     found_camera_response = detect_cameras()
     camera_ids_list_in = found_camera_response.cameras_found_list
-    
+
     asyncio.run(show_synched_frames(camera_ids_list_in))
 
     print("done!")
