26b5c2b8fb9134daca52913576a7361a27f9b9ca
mprib <prible@gmail.com>
2022-12-29 12:07:26 -0600

    track frame time

--- skellycam/examples/synchronize_frame_payloads.py
+++ skellycam/examples/synchronize_frame_payloads.py
@@ -1,11 +1,14 @@
+
 import asyncio
 import logging
 import multiprocessing
-
+import pandas as pd
+from pathlib import Path
 import cv2
 
 from skellycam.detection.detect_cameras import detect_cameras
 from skellycam.opencv.group.camera_group import CameraGroup
+from skellycam.system.environment.default_paths import default_session_folder_path
 
 logger = logging.getLogger(__name__)
 
@@ -19,16 +22,25 @@ async def show_all_cameras_in_cv2_windows(camera_ids_list: list = None):
     camera_group.start()
     should_continue = True
 
+    frame_times = {"port": [],
+                   "frame_index": [],
+                   "frame_time": [] }
+
     for p in multiprocessing.active_children():
         print(f"before big frame loop - found child process: {p}")
 
     while should_continue:
         latest_frame_payloads = camera_group.latest_frames()
-        
-        print(time.time())
         for cam_id, frame_payload in latest_frame_payloads.items():
             if frame_payload is not None:
                 cv2.imshow(f"Camera {cam_id} - Press ESC to quit", frame_payload.image)
+                frame_times["port"].append(frame_payload.camera_id)
+                frame_times["frame_index"].append(frame_payload.frame_number)
+                frame_times["frame_time"].append(frame_payload.timestamp_ns)
+        
+
+
+
         if cv2.waitKey(1) == 27:
             logger.info(f"ESC key pressed - shutting down")
             cv2.destroyAllWindows()
@@ -36,6 +48,8 @@ async def show_all_cameras_in_cv2_windows(camera_ids_list: list = None):
 
     camera_group.close()
 
+    frame_times = pd.DataFrame(frame_times)
+    frame_times.to_csv(Path(default_session_folder_path(create_folder=True),"frame_times.csv"))
 
 if __name__ == "__main__":
     found_camera_response = detect_cameras()
