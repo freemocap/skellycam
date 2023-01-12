7bc9063ce0afffa5ed4173b4b4b8a63fd5dca721
jonmatthis <jonmatthis@gmail.com>
2023-01-12 11:30:34 -0500

    move print statemetnt into `main`

--- skellycam/__main__.py
+++ skellycam/__main__.py
@@ -11,10 +11,12 @@ except Exception as e:
     sys.path.insert(0, str(base_package_path))  # add parent directory to sys.path
     from skellycam.qt_gui.qt_gui_main import qt_gui_main
 
-print(f"This is printing from {__file__}")
+
 def main():
     qt_gui_main()
 
 
 if __name__ == "__main__":
+    print(f"Running `skellycam.__main__` from - {__file__}")
+
     main()
