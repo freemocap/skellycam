e24314dce587f22d102b81cf0340f347e8bb3d9b
Jon Matthis <jonmatthis@gmail.com>
2023-01-14 20:35:34 -0500

    create queues from manager so it works on mac?

--- skellycam/opencv/group/strategies/queue_communicator.py
+++ skellycam/opencv/group/strategies/queue_communicator.py
@@ -5,12 +5,13 @@ from typing import List
 class QueueCommunicator:
     def __init__(self, identifiers: List[str]):
         self._identifiers = identifiers
+        self._mr_manager = multiprocessing.Manager()
         self._queues = self._create_queues()
 
     def _create_queues(self):
         d = {}
         for identifier in self._identifiers:
-            d.update({identifier: multiprocessing.Queue()})
+            d.update({identifier: self._mr_manager.Queue()})
         return d
 
     @property
