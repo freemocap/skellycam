58e1ad8fe38b27eeefe8aeb28ecf8334b29dfa5b
mprib <31831778+mprib@users.noreply.github.com>
2023-02-04 21:18:12 -0600

    Detect virtual cameras (#20)
    * Bump version v2023.02.1063 -> v2023.02.1064
    
    * Bump version v2023.02.1064 -> v2023.02.1065
    
    * undo accidental commits
    
    * stylistic changes. Mostly expanding shortened words and appending `_` to private methods (i.e. methods not intended to be accessed from the outside)
    
    ---------
    
    Co-authored-by: jonmatthis <jonmatthis@gmail.com>

--- skellycam/detection/private/detect_possible_cameras.py
+++ skellycam/detection/private/detect_possible_cameras.py
@@ -1,6 +1,7 @@
 import logging
 import time
 from threading import Thread
+from typing import Union
 
 import cv2
 import numpy as np
@@ -8,144 +9,143 @@ import numpy as np
 from skellycam.detection.private.found_camera_cache import FoundCameraCache
 from skellycam.opencv.config.determine_backend import determine_backend
 
-CAM_CHECK_NUM = 20  # please give me a reason to increase this number ;D
 
 logger = logging.getLogger(__name__)
 
-MIN_RESOLUTION_CHECK = 0   # the lowest width value that will be used to determine the minimum resolution of the camera
-MAX_RESOLUTION_CHECK = 10000   # the highest width value that will be used to determine the maximum resolution of the camera
-RESOLUTION_CHECK_STEPS = 10 # the number of 'slices" between the minimum and maximum resolutions that will be checked to determine the possible resolutions
+NUMBER_OF_CAMERAS_TO_CHECK = 20  # please give me a reason to increase this number ;D
+MIN_RESOLUTION_CHECK = 0  # the lowest width value that will be used to determine the minimum resolution of the camera
+MAX_RESOLUTION_CHECK = 10000  # the highest width value that will be used to determine the maximum resolution of the camera
+RESOLUTION_CHECK_STEPS = 10  # the number of 'slices" between the minimum and maximum resolutions that will be checked to determine the possible resolutions
+
 
 class DetectPossibleCameras:
-    
-    
+    def __init__(self):
+        self._cameras_to_use_list = []
+        self._video_capture_objects_list = []
+        self._camera_resolutions_dictionary = {}
+        self._assess_camera_threads = {}
+
     def find_available_cameras(self) -> FoundCameraCache:
 
-        self.cams_to_use_list = []
-        self.caps_list = []
-        self.resolutions_dict = {}
-        self.assess_camera_threads = {}
-       
-        # create a dictionary of threads that will check each port for a real camera 
-        for cam_id in range(CAM_CHECK_NUM):
-            self.assess_camera_threads[cam_id] = Thread(target=self.assess_camera, args=[cam_id,], daemon=True)
-            self.assess_camera_threads[cam_id].start()
-        
+        # create a dictionary of threads that will check each port for a real camera
+        for camera_id in range(NUMBER_OF_CAMERAS_TO_CHECK):
+            self._assess_camera_threads[camera_id] = Thread(target=self._assess_camera, args=[camera_id, ], daemon=True)
+            self._assess_camera_threads[camera_id].start()
+
         # wait for all the threads to finish
-        for key, thread in self.assess_camera_threads.items():
+        for key, thread in self._assess_camera_threads.items():
             thread.join()
 
         # due to threads, cam_ids not returned in order, so reorder       
-        self.cams_to_use_list.sort(key=int) 
-        
-        for cap in self.caps_list:
-            logger.debug(f"Releasing cap {cap}")
-            cap.release()
-            logger.debug(f"Deleting cap {cap}")
-            del cap
+        self._cameras_to_use_list.sort(key=int)
 
-        logger.debug(f"Deleting caps_list {self.caps_list}")
-        del self.caps_list
-        
+        for video_capture_object in self._video_capture_objects_list:
+            logger.debug(f"Releasing cap {video_capture_object}")
+            video_capture_object.release()
+            logger.debug(f"Deleting cap {video_capture_object}")
+            del video_capture_object
 
-        logger.info(f"Found cameras: {self.cams_to_use_list}")
+        logger.debug(f"Deleting caps_list {self._video_capture_objects_list}")
+        del self._video_capture_objects_list
+
+        logger.info(f"Found cameras: {self._cameras_to_use_list}")
         return FoundCameraCache(
-            number_of_cameras_found=len(self.cams_to_use_list),
-            cameras_found_list=self.cams_to_use_list,
-            possible_resolutions=self.resolutions_dict
+            number_of_cameras_found=len(self._cameras_to_use_list),
+            cameras_found_list=self._cameras_to_use_list,
+            possible_resolutions=self._camera_resolutions_dictionary
         )
 
-    def assess_camera(self,cam_id):
+    def _assess_camera(self, camera_id: Union[str, int]):
         """A worker that can be spun up on it's own thread to sort out whether or not a given port connects to a legitimate camera"""
-        cap = cv2.VideoCapture(cam_id)
-        success, image1 = cap.read()
-        time0 = time.perf_counter()
+        video_capture_object = cv2.VideoCapture(camera_id)
+        success, image1 = video_capture_object.read()
 
         if not success:
             return
 
         if image1 is None:
             return
-        
-        possible_resolutions = self.get_possible_resolutions(cap, cam_id)
+
+        possible_resolutions = self._get_possible_resolutions(video_capture_object, camera_id)
 
         if len(possible_resolutions) == 1:
             logger.debug(
-                f"Camera {cam_id} has only one possible resolution...likely virtual"
+                f"Camera {camera_id} has only one possible resolution...likely virtual"
             )
-        else: 
+        else:
             logger.debug(
-                f"Camera found at port number {cam_id}: success={success}, "
-                f"image.shape={image1.shape},  cap={cap}"
+                f"Camera found at port number {camera_id}: success={success}, "
+                f"image.shape={image1.shape},  cap={video_capture_object}"
             )
-            
-            self.cams_to_use_list.append(str(cam_id))
-            self.caps_list.append(cap)
-            self.resolutions_dict[cam_id] = possible_resolutions
-        
-    def get_nearest_resolution(self, video_capture, target_width):
+
+            self._cameras_to_use_list.append(str(camera_id))
+            self._video_capture_objects_list.append(video_capture_object)
+            self._camera_resolutions_dictionary[camera_id] = possible_resolutions
+
+    def _get_nearest_resolution(self, video_capture: cv2.VideoCapture, target_width: float):
         """
         returns the resolution nearest the target width for a given cv2.VideoCapture object
         """
-        
+
         # 1. store the current resolution of the VideoCapture object to reset it later
         current_width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-        current_height =int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        
+        current_height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
         # 2. attempt to set its width to the provided target_width
         video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, int(target_width))
-        
+
         # 3. determine which resolution the VideoCapture object is actually able to attain
         nearest_width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
         nearest_height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        
+
         # 4. reset the VideoCapture object to the original resolution
         video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, current_width)
         video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, current_height)
 
-        logger.info(f"Resolution with a width nearest {target_width} is {(nearest_width,nearest_height)}")
+        logger.info(f"Resolution with a width nearest {target_width} is {(nearest_width, nearest_height)}")
 
         # 5. return the resolution as a tuple of (width, height)
-        return (nearest_width, nearest_height) 
+        return (nearest_width, nearest_height)
 
-    def get_possible_resolutions(self, video_capture, cam_id):
-        
-        min_res = self.get_nearest_resolution(video_capture, MIN_RESOLUTION_CHECK)
-        max_res = self.get_nearest_resolution(video_capture, MAX_RESOLUTION_CHECK)
-        logger.info(f"Minimum resolution of camera at port {cam_id} is {min_res}")
-        logger.info(f"Maximum resolution of camera at port {cam_id} is {max_res}")
+    def _get_possible_resolutions(self, video_capture: cv2.VideoCapture, cam_id: Union[str, int]):
 
-        min_width = min_res[0]
-        max_width = max_res[0]
+        minimum_resolution = self._get_nearest_resolution(video_capture, MIN_RESOLUTION_CHECK)
+        maximum_resolution = self._get_nearest_resolution(video_capture, MAX_RESOLUTION_CHECK)
+        logger.info(f"Minimum resolution of camera at port {cam_id} is {minimum_resolution}")
+        logger.info(f"Maximum resolution of camera at port {cam_id} is {maximum_resolution}")
 
-        STEPS_TO_CHECK = 10  # fast to check so cover your bases
+        minimum_width = minimum_resolution[0]
+        maximum_width = maximum_resolution[0]
+
+        steps_to_check = 10  # fast to check so cover your bases
 
         # the size of jump to make before checking on the resolution
-        step_size = int((max_width - min_width) / STEPS_TO_CHECK)
+        step_size = int((maximum_width - minimum_width) / steps_to_check)
 
-        resolutions = {min_res, max_res}
+        resolutions = {minimum_resolution, maximum_resolution}
 
-        if max_width > min_width:  # i.e. only one size avaialable
+        if maximum_width > minimum_width:  # i.e. only one size avaialable
             for test_width in range(
-                int(min_width + step_size), int(max_width - step_size), int(step_size)
+                    int(minimum_width + step_size), int(maximum_width - step_size), int(step_size)
             ):
-                new_res = self.get_nearest_resolution(video_capture, test_width)
+                new_res = self._get_nearest_resolution(video_capture, test_width)
                 # print(new_res)
                 resolutions.add(new_res)
             resolutions = list(resolutions)
             resolutions.sort()
             possible_resolutions = resolutions
         else:
-            possible_resolutions = [min_res]
+            possible_resolutions = [minimum_resolution]
 
         logger.info(f"At port {cam_id} the possible resolutions are {possible_resolutions}")
-        
+
         return possible_resolutions
+
 
 if __name__ == "__main__":
     detector = DetectPossibleCameras()
     start_time = time.perf_counter()
     camera_cache = detector.find_available_cameras()
-    elapsed_time = time.perf_counter()-start_time
-    
-    print(f"Elapsed time for camera detection is {round(elapsed_time,2)} seconds")
+    elapsed_time = time.perf_counter() - start_time
+
+    print(f"Elapsed time for camera detection is {round(elapsed_time, 2)} seconds")
