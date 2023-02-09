99a0c46eb66dad3a7fc004731710b1ec438474f9
jonmatthis <jonmatthis@gmail.com>
2023-02-09 16:17:57 -0500

    Bump version v2023.02.1073 -> v2023.02.1074

--- skellycam/__init__.py
+++ skellycam/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Skelly FreeMoCap"""
 __email__ = "info@freemocap.org"
-__version__ = "v2023.02.1073"
+__version__ = "v2023.02.1074"
 
 __description__ = "A simple python API for efficiently watching camera streams 💀📸"
 __package_name__ = "skellycam"
