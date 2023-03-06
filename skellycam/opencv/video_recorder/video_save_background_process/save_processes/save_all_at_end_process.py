239c9db412b1c5c6ce7e45f996d683563a519069
Endurance Idehen <jeidehen@gmail.com>
2023-03-06 09:28:59 -0800

    add review and code refactor

--- skellycam/opencv/video_recorder/video_save_background_process/save_processes/save_all_at_end_process.py
+++ skellycam/opencv/video_recorder/video_save_background_process/save_processes/save_all_at_end_process.py
@@ -3,73 +3,87 @@ import multiprocessing
 import time
 from copy import deepcopy
 from pathlib import Path
-from time import sleep
 from typing import List, Dict
 
 import numpy as np
 
 from skellycam.detection.models.frame_payload import FramePayload
-from skellycam.opencv.video_recorder.save_synchronized_videos import save_synchronized_videos
+from skellycam.opencv.camera.types.camera_id import CameraId
+from skellycam.opencv.video_recorder.save_synchronized_videos import (
+    save_synchronized_videos,
+)
 from skellycam.opencv.video_recorder.video_recorder import VideoRecorder
-
 
 logger = logging.getLogger(__name__)
 
 
-def save_all_at_end_process(frame_lists_by_camera: Dict[str, List[FramePayload]],
-                            folder_to_save_videos: List[str],
-                            dump_frames_to_video_event: multiprocessing.Event,
-                            ):
-    logger.info("Starting VideoSaveBackgroundProcess")
-    video_recorders = {}
+class SaveAll:
+    def __init__(
+        self,
+        frame_lists_by_camera: Dict[str, List[FramePayload]],
+        folder_to_save_videos: List[str],
+        dump_frames_to_video_event: multiprocessing.Event,
+    ):
+        self._frame_lists_by_camera = frame_lists_by_camera
+        self._folder_to_save_videos = folder_to_save_videos
+        self._dump_frames_to_video_event = dump_frames_to_video_event
+        self._video_recorders: Dict[CameraId, VideoRecorder] = {}
 
-    while True:
-        sleep(1)
+    def run(self):
         # logger.debug("Video Save Process - Checking if frames need to be saved...")
-        if dump_frames_to_video_event.is_set():
-
+        if self._dump_frames_to_video_event.is_set():
             logger.info("Video Save Process - There are frames to save!")
 
             logger.debug("Clearing dump_frames_to_video_event...")
-            dump_frames_to_video_event.clear()
+            self._dump_frames_to_video_event.clear()
 
-            for camera_id, frame_list in frame_lists_by_camera.items():
-                logger.info(f"VIDEO SAVE PROCESS - {camera_id} has {len(frame_list)} frames in the list")
+            for camera_id, frame_list in self._frame_lists_by_camera.items():
+                logger.info(
+                    f"VIDEO SAVE PROCESS - {camera_id} has {len(frame_list)} frames in the list"
+                )
 
-                video_recorders[camera_id] = VideoRecorder()
+                self._video_recorders[camera_id] = VideoRecorder()
 
                 tik = time.perf_counter()
-                video_recorders[camera_id].frame_list = deepcopy(frame_list[1:])
-                deepcopy_duration = time.perf_counter()- tik
+                self._video_recorders[camera_id].frame_list = deepcopy(frame_list[1:])
+                deepcopy_duration = time.perf_counter() - tik
 
-                logger.debug(f" Camera {camera_id} - deepcopy_duration: {deepcopy_duration:.4f}")
+                logger.debug(
+                    f" Camera {camera_id} - deepcopy_duration: {deepcopy_duration:.4f}"
+                )
 
-                if len(video_recorders[camera_id].frame_list) == 0:
-                    logger.error(f"VIDEO SAVE PROCESS - {camera_id} has no frames to save")
-                    raise Exception(f"VIDEO SAVE PROCESS - {camera_id} has no frames to save")
+                if len(self._video_recorders[camera_id].frame_list) == 0:
+                    logger.error(
+                        f"VIDEO SAVE PROCESS - {camera_id} has no frames to save"
+                    )
+                    raise Exception(
+                        f"VIDEO SAVE PROCESS - {camera_id} has no frames to save"
+                    )
 
-                if np.isinf(video_recorders[camera_id].frames_per_second):
-                    raise Exception(f"VIDEO SAVE PROCESS - {camera_id} frames_per_second is inf")
+                if np.isinf(self._video_recorders[camera_id].frames_per_second):
+                    raise Exception(
+                        f"VIDEO SAVE PROCESS - {camera_id} frames_per_second is inf"
+                    )
 
-                logger.info(f"VideoRecorder {camera_id} - {video_recorders[camera_id].number_of_frames} frames at {video_recorders[camera_id].frames_per_second} fps")
+                logger.info(
+                    f"VideoRecorder {camera_id} - "
+                    f"{self._video_recorders[camera_id].number_of_frames} "
+                    f"frames at {self._video_recorders[camera_id].frames_per_second} fps"
+                )
 
             logger.debug(
-                f"Saving frames to video files - {[video_recorder.number_of_frames for video_recorder in video_recorders.values()]}...")
+                f"Saving frames to video files - "
+                f"{[video_recorder.number_of_frames for video_recorder in self._video_recorders.values()]}..."
+            )
 
-
-
-            folder_to_save_videos_path = str(Path(folder_to_save_videos.pop()))
+            folder_to_save_videos_path = str(Path(self._folder_to_save_videos.pop()))
 
             save_synchronized_videos(
-                raw_video_recorders=video_recorders,
+                raw_video_recorders=self._video_recorders,
                 folder_to_save_videos=folder_to_save_videos_path,
                 create_diagnostic_plots_bool=True,
             )
 
             logger.info(
-                f"`Saved synchronized videos to folder: {folder_to_save_videos_path}")
-
-
-        if not multiprocessing.parent_process().is_alive():
-            logger.info("Parent process is dead. Exiting")
-            break
+                f"`Saved synchronized videos to folder: {folder_to_save_videos_path}"
+            )
--- skellycam/opencv/video_recorder/video_save_background_process/video_save_background_process.py
+++ skellycam/opencv/video_recorder/video_save_background_process/video_save_background_process.py
@@ -1,43 +1,69 @@
 import logging
 import multiprocessing
 from multiprocessing import Process
+from time import sleep
 from typing import Dict, List
 
 from skellycam.detection.models.frame_payload import FramePayload
-from skellycam.opencv.video_recorder.video_save_background_process.save_processes.save_all_at_end_process import \
-    save_all_at_end_process
+from skellycam.opencv.video_recorder.video_save_background_process.save_processes.save_all_at_end_process import (
+    SaveAll,
+)
+from skellycam.opencv.video_recorder.video_save_background_process.save_processes.save_per_chunk_process import (
+    ChunkSave,
+)
 
 logger = logging.getLogger(__name__)
 
 
 class VideoSaveBackgroundProcess:
-
-    def __init__(self,
-                 frame_lists_by_camera: Dict[str, List[FramePayload]],
-                 folder_to_save_videos: List[str],
-                 dump_frames_to_video_event: multiprocessing.Event,
-
-                 ):
-
+    def __init__(
+        self,
+        frame_lists_by_camera: Dict[str, List[FramePayload]],
+        folder_to_save_videos: List[str],
+        dump_frames_to_video_event: multiprocessing.Event,
+    ):
         self._process = Process(
             name="VideoSaveBackgroundProcess",
-            # target=save_per_chunk_process,
+            # target=self._start_save_chunk,
             target=self._start_save_all_at_end_process,
-            args=(frame_lists_by_camera,
-                  folder_to_save_videos,
-                  dump_frames_to_video_event),
+            args=(
+                frame_lists_by_camera,
+                folder_to_save_videos,
+                dump_frames_to_video_event,
+            ),
         )
 
     @staticmethod
-    def _start_save_all_at_end_process(frame_lists_by_camera: Dict[str, List[FramePayload]],
-                                       folder_to_save_videos: List[str],
-                                       dump_frames_to_video_event: multiprocessing.Event,
-                                       ):
-        save_all_at_end_process(
-            frame_lists_by_camera=frame_lists_by_camera,
-            folder_to_save_videos=folder_to_save_videos,
-            dump_frames_to_video_event=dump_frames_to_video_event,
+    def _start_save_all_at_end_process(
+        frame_lists_by_camera: Dict[str, List[FramePayload]],
+        folder_to_save_videos: List[str],
+        dump_frames_to_video_event: multiprocessing.Event,
+    ):
+        sa = SaveAll(
+            frame_lists_by_camera, folder_to_save_videos, dump_frames_to_video_event
         )
+        while True:
+            sleep(1)
+            sa.run()
+            # TODO: Processes should be managed by the parent.
+            #  We can set up the SIGINT signal in children to ensure daemon processes
+            #  respond appropriately.
+            if not multiprocessing.parent_process().is_alive():
+                logger.info("Parent process is dead. Exiting")
+                break
+
+    @staticmethod
+    def _start_save_chunk(
+        frame_lists_by_camera: Dict[str, List[FramePayload]],
+        folder_to_save_videos: List[str],
+        dump_frames_to_video_event: multiprocessing.Event,
+    ):
+        cs = ChunkSave(
+            frame_lists_by_camera, folder_to_save_videos, dump_frames_to_video_event
+        )
+        while True:
+            sleep(1)
+            cs.run()
 
     @property
     def is_alive(self):
