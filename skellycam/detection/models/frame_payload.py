f8fb2341a5aa649b2ce355d9b1d33e3c2d385016
jonmatthis <jonmatthis@gmail.com>
2023-06-21 09:33:29 -0400

    fix dumb double definition bug yikes

--- skellycam/detection/models/frame_payload.py
+++ skellycam/detection/models/frame_payload.py
@@ -3,16 +3,16 @@ import dataclasses
 import numpy as np
 
 
-# TODO: This shouldn't be a dataclass. Use __slots__
-# https://stackoverflow.com/questions/472000/usage-of-slots
-@dataclasses.dataclass()
-class FramePayload:
-    success: bool = False
-    image: np.ndarray = None
-    # using nanoseconds to avoid floating point inprecision -  divide by `1e9` to get seconds
-    timestamp_ns: int = None
-    camera_id: str = None
-
+# # TODO: This shouldn't be a dataclass. Use __slots__
+# # https://stackoverflow.com/questions/472000/usage-of-slots
+# @dataclasses.dataclass()
+# class FramePayload:
+#     success: bool = False
+#     image: np.ndarray = None
+#     # using nanoseconds to avoid floating point inprecision -  divide by `1e9` to get seconds
+#     timestamp_ns: int = None
+#     camera_id: str = None
+#
 
 import numpy as np
 
@@ -37,9 +37,12 @@ class FramePayload:
 
     """
 
-    __slots__ = ['success', 'image', 'timestamp_ns', 'camera_id']
+    __slots__ = ['success', 'image', 'timestamp_ns', 'camera_id', 'recorded']
 
-    def __init__(self, success: bool = False, image: np.ndarray = None, timestamp_ns: int = None,
+    def __init__(self,
+                 success: bool = False,
+                 image: np.ndarray = None,
+                 timestamp_ns: int = None,
                  camera_id: str = None):
         self.success = success
         self.image = image
