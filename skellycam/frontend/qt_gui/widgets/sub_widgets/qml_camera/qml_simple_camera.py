52360c3ea142db1191c5f8e9d422187f18e6b896
jonmatthis <jonmatthis@gmail.com>
2023-10-01 15:37:26 -0400

    simple qml poc

---
+++ skellycam/frontend/qt_gui/widgets/sub_widgets/qml_camera/qml_simple_camera.py
@@ -0,0 +1,25 @@
+import sys
+
+from PySide6.QtCore import QUrl
+from PySide6.QtQuickWidgets import QQuickWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+
+    # QQuickWidget for embedding QML
+    qml_widget = QQuickWidget()
+    qml_widget.setSource(QUrl('simple_camera.qml'))
+    qml_widget.setResizeMode(QQuickWidget.SizeRootObjectToView)
+
+    # Embed QML in QWidget
+    main_widget = QWidget()
+    main_widget.setLayout(QVBoxLayout())
+    main_widget.layout().addWidget(qml_widget)
+
+    # Set the widget as the central widget of the QMainWindow
+    window.setCentralWidget(main_widget)
+    window.show()
+
+    sys.exit(app.exec())
