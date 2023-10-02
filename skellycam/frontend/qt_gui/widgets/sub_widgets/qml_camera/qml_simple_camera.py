1a77098a196598f870778de9c0ab6094ce3274af
jonmatthis <jonmatthis@gmail.com>
2023-10-02 05:42:01 -0400

    simple camera v simple

--- skellycam/frontend/qt_gui/widgets/sub_widgets/qml_camera/qml_simple_camera.py
+++ skellycam/frontend/qt_gui/widgets/sub_widgets/qml_camera/qml_simple_camera.py
@@ -1,15 +1,13 @@
+import logging
 import sys
 from pathlib import Path
 
-from PySide6.QtCore import QUrl, Slot
-from PySide6.QtQml import QQmlError
+from PySide6.QtCore import QUrl
 from PySide6.QtQuickWidgets import QQuickWidget
 from PySide6.QtWidgets import QApplication, QVBoxLayout, QWidget
 
-import logging
-
-
 logger = logging.getLogger(__name__)
+
 
 class QMLCamera(QWidget):
     def __init__(self):
@@ -17,20 +15,16 @@ class QMLCamera(QWidget):
 
         # QQuickWidget for embedding QML
         self.qml_widget = QQuickWidget()
-        camera_view_qml_path = str(Path(__file__).parent / "declarative-camera.qml")
+        camera_view_qml_path = str(Path(__file__).parent / "single_camera.qml")
         self.qml_widget.setSource(QUrl.fromLocalFile(camera_view_qml_path))
         self.qml_widget.setResizeMode(QQuickWidget.SizeRootObjectToView)
-
 
         # Embed QML in QWidget
         self.setLayout(QVBoxLayout())
         self.layout().addWidget(self.qml_widget)
 
 
-
-
 if __name__ == "__main__":
-
     app = QApplication(sys.argv)
 
     widget = QMLCamera()
