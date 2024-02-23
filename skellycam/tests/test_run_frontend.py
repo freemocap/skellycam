b93d184367d09cca7a198427d7b3e1fb02974eeb
jonmatthis <jonmatthis@gmail.com>
2024-02-23 16:24:18 -0500

    test-frontend

---
+++ skellycam/tests/test_run_frontend.py
@@ -0,0 +1,24 @@
+from pytest_mock import MockerFixture
+import multiprocessing
+from multiprocessing import Process
+
+from skellycam.frontend.run_frontend import run_frontend
+
+
+def test_run_frontend_starts_process(mocker: MockerFixture) -> None:
+    # Mock the run_qt_application function to prevent the actual GUI from running
+    mocker.patch(
+        "skellycam.frontend.qt_application.run_qt_application", return_value=None
+    )
+
+    reboot_event: multiprocessing.Event = multiprocessing.Event()
+    shutdown_event: multiprocessing.Event = multiprocessing.Event()
+
+    frontend_process: Process = run_frontend(
+        "localhost", 8000, 5, reboot_event, shutdown_event
+    )
+
+    assert frontend_process.is_alive() == True
+    # Add more assertions if necessary
+
+    frontend_process.terminate()  # Make sure to terminate the process after the test
