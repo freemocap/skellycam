38d86e4338d040de1256ca37a5fdf23f30cf6895
jonmatthis <jonmatthis@gmail.com>
2024-03-20 09:52:05 -0400

    working webcam component

--- skellycam/experiments/nuxt-tauri-cam/nuxt.config.ts
+++ skellycam/experiments/nuxt-tauri-cam/nuxt.config.ts
@@ -15,18 +15,18 @@ export default defineNuxtConfig({
     // Enable environment variables
     // Additional environment variables can be found at
     // https://tauri.app/2/reference/environment-variables/
-    envPrefix: ['VITE_', 'TAURI_'],
+    envPrefix: ["VITE_", "TAURI_"],
     server: {
       // Tauri requires a consistent port
       strictPort: true,
       // Enables the development server to be discoverable by other devices for mobile development
       // @ts-ignore
-      host: '0.0.0.0',
+      host: "0.0.0.0",
       hmr: {
         // Use websocket for mobile hot reloading
-        protocol: 'ws',
+        protocol: "ws",
         // Make sure it's available on the network
-        host: '0.0.0.0',
+        host: "0.0.0.0",
         // Use a specific port for hmr
         port: 5183,
       },
--- skellycam/experiments/nuxt-tauri-cam/pages/index.vue
+++ skellycam/experiments/nuxt-tauri-cam/pages/index.vue
@@ -1,34 +1,5 @@
 <template>
-  <div class="webcam-viewer">
-    <video ref="videoElement" autoplay></video>
+  <div>
+    <WebcamPage />
   </div>
 </template>
-
-<script setup>
-import { onMounted, ref } from 'vue';
-
-const videoElement = ref(null);
-
-const startWebcam = async () => {
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-    if(videoElement.value) {
-      videoElement.value.srcObject = stream;
-    }
-  } catch (err) {
-    console.error('Error accessing the webcam:', err);
-  }
-};
-
-onMounted(() => {
-  startWebcam();
-});
-</script>
-
-<style>
-.webcam-viewer video {
-  width: 90vw;
-  height: 90vh;
-  object-fit: contain;
-}
-</style>
