a5760385bca0cc58b233685bd8d80641d37b7546
jonmatthis <jonmatthis@gmail.com>
2024-05-07 06:57:32 -0400

    pprint

--- skellycam/core/cameras/config/camera_configs.py
+++ skellycam/core/cameras/config/camera_configs.py
@@ -17,8 +17,40 @@ class CameraConfigs(RootModel):
         for camera_id, config in self.root.items():
             out_str += f"Camera {camera_id}:\n"
             out_str += "\t" + str(config).replace("\n", "\n\t")
+            out_str += "\n"
+        # remove the last newline
+        out_str = out_str[:-3]
         return out_str
+
+    def __getitem__(self, item):
+        return self.root[item]
+
+    def __setitem__(self, key, value):
+        self.root[key] = value
+
+    def __delitem__(self, key):
+        del self.root[key]
+
+    def __iter__(self):
+        return iter(self.root)
+
+    def __len__(self):
+        return len(self.root)
+
+    def __contains__(self, item):
+        return item in self.root
+
+    def __eq__(self, other):
+        return self.root == other.root
+
+    def keys(self):
+        return self.root.keys()
+
+    def values(self):
+        return self.root.values()
 
 
 if __name__ == "__main__":
-    print(CameraConfigs())
+    configs = CameraConfigs()
+    configs[1] = CameraConfig(camera_id=CameraId(1))
+    print(configs)
