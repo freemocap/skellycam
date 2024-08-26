e2f7034bf378b696b48e607208868af512f7c2cf
jonmatthis <jonmatthis@gmail.com>
2024-08-26 07:30:25 -0400

    runs smoothly and rec's

--- skellycam/api/app/controller_tasks.py
+++ skellycam/api/app/controller_tasks.py
@@ -1,23 +1,15 @@
 import asyncio
 import logging
-
-from skellycam.api.app.app_state import get_app_state, TaskStatus
+from typing import Optional
 
 logger = logging.getLogger(__name__)
 
 
 class ControllerTasks:
     def __init__(self):
-        self._task_awaiters: list[asyncio.Task] = []
-
-        self._connect_to_cameras_task: asyncio.Task = asyncio.create_task(asyncio.sleep(0), name="ConnectToCameras")
-        self._detect_available_cameras_task: asyncio.Task = asyncio.create_task(asyncio.sleep(0),
-                                                                                name="DetectAvailableCameras")
-        self._close_cameras_task: asyncio.Task = asyncio.create_task(asyncio.sleep(0), name="CloseCameras")
-
-        self._add_task_awaiter(self._connect_to_cameras_task)
-        self._add_task_awaiter(self._detect_available_cameras_task)
-        self._add_task_awaiter(self._close_cameras_task)
+        self._connect_to_cameras_task: Optional[asyncio.Task] = None
+        self._detect_available_cameras_task: Optional[asyncio.Task] = None
+        self._close_cameras_task: Optional[asyncio.Task] = None
 
     @property
     async def connect_to_cameras_task(self) -> asyncio.Task:
@@ -27,7 +19,6 @@ class ControllerTasks:
     def connect_to_cameras_task(self, value: asyncio.Task) -> None:
         if self._connect_to_cameras_task is None or self._connect_to_cameras_task.done():
             self._connect_to_cameras_task = value
-            self._add_task_awaiter(self._connect_to_cameras_task)
             return
         else:
             logger.warning("Camera connection task already running! Ignoring request...")
@@ -40,7 +31,6 @@ class ControllerTasks:
     def detect_available_cameras_task(self, value: asyncio.Task) -> None:
         if self._detect_available_cameras_task is None or self._detect_available_cameras_task.done():
             self._detect_available_cameras_task = value
-            self._add_task_awaiter(self._detect_available_cameras_task)
             return
         else:
             logger.warning("Camera detection task already running! Ignoring request...")
@@ -53,18 +43,8 @@ class ControllerTasks:
     def close_cameras_task(self, value: asyncio.Task) -> None:
         if self._close_cameras_task is None or self._close_cameras_task.done():
             self._close_cameras_task = value
-            self._add_task_awaiter(self._close_cameras_task)
             return
         else:
             logger.warning("Camera close task already running! Ignoring request...")
 
-    def _add_task_awaiter(self, task: asyncio.Task) -> None:
-        self._task_awaiters.append(asyncio.create_task(await_task(task)))
 
-
-async def await_task(task: asyncio.Task) -> None:
-    if not task:
-        raise ValueError("Task is None!")
-    get_app_state().update_task_status(TaskStatus.from_task(task))
-    await task
-    get_app_state().update_task_status(TaskStatus.from_task(task))
