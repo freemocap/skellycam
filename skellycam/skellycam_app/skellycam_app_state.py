9ca828a339dd814378d7daa6fcccd9969a4cf9be
jonmatthis <jonmatthis@gmail.com>
2024-12-20 09:10:32 -0500

    dont pull from root

--- skellycam/skellycam_app/skellycam_app_state.py
+++ skellycam/skellycam_app/skellycam_app_state.py
@@ -6,8 +6,8 @@ from typing import Optional
 from uuid import uuid4
 
 from pydantic import BaseModel
+from skellycam.core import CameraId
 
-from skellycam import CameraId
 from skellycam.core.camera_group.camera.config.camera_config import CameraConfigs
 from skellycam.core.camera_group.camera.config.update_instructions import UpdateInstructions
 from skellycam.core.camera_group.camera_group import CameraGroup
