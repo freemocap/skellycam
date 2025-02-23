afdaa7cf60f4e55cb70928f00b2bfdd5aa80ea5f
jonmatthis <jonmatthis@gmail.com>
2025-02-23 08:58:16 -0500

    xcv

--- installers/nuitka_scripts/nuitka_installer_mac.sh
+++ installers/nuitka_scripts/nuitka_installer_mac.sh
@@ -3,5 +3,5 @@
 # Get the directory of the script
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Script is located in: $script_dir"
-
+brew install portaudio
 nuitka --onefile --macos-create-app-bundle=1 --macos-app-icon="$script_dir/../shared/skellycam-logo/skellycam-favicon.ico" --user-package-configuration-file="$script_dir/../installers/skellycam-nuitka.config.yml" --output-filename="skellycam_server" "$script_dir/../skellycam/__main__.py"
