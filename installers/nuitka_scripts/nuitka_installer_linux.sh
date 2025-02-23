94e13d10cfd1ad5f28022e152c5992c6bef8a008
jonmatthis <jonmatthis@gmail.com>
2025-02-23 08:39:40 -0500

    eriofjg

--- installers/nuitka_scripts/nuitka_installer_linux.sh
+++ installers/nuitka_scripts/nuitka_installer_linux.sh
@@ -5,5 +5,5 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Script is located in: $script_dir"
 
 sudo apt-get update
-sudo apt-get install -y clang portaudio19-dev
+#sudo apt-get install -y clang portaudio19-dev
 nuitka --onefile --linux-icon="$script_dir/../shared/skellycam-logo/skellycam-favicon.ico" --user-package-configuration-file="$script_dir/../installers/skellycam-nuitka.config.yml" "$script_dir/../skellycam/__main__.py"
