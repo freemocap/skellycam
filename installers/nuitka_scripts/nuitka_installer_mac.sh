97eacc0dc758bde9a39cde66bc62dbab7a65ec8c
philipqueen <queen.philip@gmail.com>
2025-02-26 11:14:45 -0700

    changes for mac app

--- installers/nuitka_scripts/nuitka_installer_mac.sh
+++ installers/nuitka_scripts/nuitka_installer_mac.sh
@@ -4,4 +4,4 @@
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Script is located in: $script_dir"
 brew install portaudio
-nuitka --onefile --macos-create-app-bundle=1 --macos-app-icon="$script_dir/../shared/skellycam-logo/skellycam-favicon.ico" --user-package-configuration-file="$script_dir/../installers/skellycam-nuitka.config.yml" --output-filename="skellycam_server" "$script_dir/../skellycam/__main__.py"
+nuitka --onefile --macos-create-app-bundle --macos-app-icon="$script_dir/../../shared/skellycam-logo/skellycam-favicon.ico" --user-package-configuration-file="$script_dir/skellycam-nuitka.config.yml" --output-filename="skellycam_server" "$script_dir/../../skellycam/__main__.py"
--- skellycam-ui/electron/main/helpers/app-paths.ts
+++ skellycam-ui/electron/main/helpers/app-paths.ts
@@ -6,8 +6,15 @@ export const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export const APP_PATHS = {
     PRELOAD: path.join(__dirname, '../preload/index.mjs'),
     RENDERER_HTML: path.join(__dirname, '../../dist/index.html'),
-    PYTHON_SERVER_EXECUTABLE_PATH: path.resolve(process.resourcesPath, 'app.asar.unpacked/skellycam_server.exe'),
-    PYTHON_SERVER_EXECUTABLE_DEV: path.resolve(__dirname, '../../skellycam_server.exe'),
+    PYTHON_SERVER_EXECUTABLE_PATH: path.resolve(
+        process.resourcesPath, 
+        'app.asar.unpacked', 
+        process.platform === 'win32' ? 'skellycam_server.exe' : 'skellycam_server'
+    ),
+    PYTHON_SERVER_EXECUTABLE_DEV: path.resolve(
+        __dirname, 
+        '../../', 
+        process.platform === 'win32' ? 'skellycam_server.exe' : 'skellycam_server'
+    ),
     SKELLYCAM_ICON_PATH: path.resolve(__dirname, '../../../shared/skellycam-logo/skellycam-favicon.ico')
-
 };
