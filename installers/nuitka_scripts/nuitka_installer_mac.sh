03b858cc337919e8621e1b6d783388a03ef9e887
philipqueen <queen.philip@gmail.com>
2025-02-26 13:07:52 -0700

    auto move skellycam_server file to skellycam-ui

--- installers/nuitka_scripts/nuitka_installer_mac.sh
+++ installers/nuitka_scripts/nuitka_installer_mac.sh
@@ -5,3 +5,4 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Script is located in: $script_dir"
 brew install portaudio
 nuitka --onefile --macos-create-app-bundle --macos-app-icon="$script_dir/../../shared/skellycam-logo/skellycam-favicon.ico" --user-package-configuration-file="$script_dir/skellycam-nuitka.config.yml" --output-filename="skellycam_server" "$script_dir/../../skellycam/__main__.py"
+mv skellycam_server "$script_dir/../../skellycam-ui/skellycam_server"
