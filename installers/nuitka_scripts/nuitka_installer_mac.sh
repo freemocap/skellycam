d73254dddf26a0756afe828c0153ce22a489cce9
philipqueen <queen.philip@gmail.com>
2025-02-27 09:40:43 -0700

    try fixing installer action

--- installers/nuitka_scripts/nuitka_installer_mac.sh
+++ installers/nuitka_scripts/nuitka_installer_mac.sh
@@ -5,4 +5,3 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Script is located in: $script_dir"
 brew install portaudio
 nuitka --onefile --user-package-configuration-file="$script_dir/skellycam-nuitka.config.yml" --output-filename="skellycam_server" "$script_dir/../../skellycam/__main__.py"
-mv skellycam_server "$script_dir/../../skellycam-ui/skellycam_server"
