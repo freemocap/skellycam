75ece9f04a3ae7428e36b464a4e452f640b50f9c
philipqueen <queen.philip@gmail.com>
2025-02-27 22:18:50 -0700

    replace -y with --assume-yes-for-downloads

--- installers/nuitka_scripts/nuitka_installer_mac.sh
+++ installers/nuitka_scripts/nuitka_installer_mac.sh
@@ -4,4 +4,4 @@
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Script is located in: $script_dir"
 brew install portaudio
-nuitka --onefile --disable-ccache --user-package-configuration-file="$script_dir/skellycam-nuitka.config.yml" --output-filename="skellycam_server" "$script_dir/../../skellycam/__main__.py" -y
+nuitka --onefile --disable-ccache --user-package-configuration-file="$script_dir/skellycam-nuitka.config.yml" --output-filename="skellycam_server" "$script_dir/../../skellycam/__main__.py" --assume-yes-for-downloads
--- installers/nuitka_scripts/nuitka_installer_windows.bat
+++ installers/nuitka_scripts/nuitka_installer_windows.bat
@@ -3,4 +3,4 @@
 set script_dir=%~dp0
 echo Script is located in: %script_dir%
 
-nuitka --onefile --windows-icon-from-ico=%script_dir%..\..\shared\skellycam-logo\skellycam-favicon.ico --user-package-configuration-file=%script_dir%skellycam-nuitka.config.yml --remove-output --output-filename=skellycam_server.exe %script_dir%..\..\skellycam\__main__.py -y
+nuitka --onefile --windows-icon-from-ico=%script_dir%..\..\shared\skellycam-logo\skellycam-favicon.ico --user-package-configuration-file=%script_dir%skellycam-nuitka.config.yml --remove-output --output-filename=skellycam_server.exe %script_dir%..\..\skellycam\__main__.py --assume-yes-for-downloads
