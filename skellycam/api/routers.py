ce2be4b7b6185ea2e3f24f314794b4d2e3b90de0
jonmatthis <jonmatthis@gmail.com>
2025-05-12 16:16:49 -0400

    adding videos endpoints

--- skellycam/api/routers.py
+++ skellycam/api/routers.py
@@ -1,12 +1,16 @@
 from skellycam.api.http.cameras.cameras_connect_router import connect_cameras_router
 from skellycam.api.http.cameras.cameras_record_router import record_cameras_router
+from skellycam.api.http.videos.videos_router import load_videos_router
 
 SKELLYCAM_ROUTERS = {
     "/skellycam": {
-        "connect": connect_cameras_router,
-        # "configs": camera_configs_router,
-        "record": record_cameras_router,
-        # "close": close_cameras_router
+        "cameras": {
+            "connect": connect_cameras_router,
+            "record": record_cameras_router
+        },
+        "videos" : {
+            "load": load_videos_router,
+        }
     },
 
 }
