9547936018a734736e386f3ebf6cbafb06e1bced
jonmatthis <jonmatthis@gmail.com>
2025-08-08 14:18:21 -0400

    pfffffffffffffffff

--- skellycam-ui/src/App.tsx
+++ skellycam-ui/src/App.tsx
@@ -4,14 +4,17 @@ import { Provider } from "react-redux";
 import { AppStateStore } from "@/store/AppStateStore";
 import { WebSocketContextProvider } from "@/context/websocket-context/WebSocketContext";
 import { urlService } from '@/services/urlService';
+import { LmdbContextProvider } from './context/lmdb-context/LmdbContext';
 
 function App() {
     const wsUrl = urlService.getWebSocketUrl();
     return (
         <Provider store={AppStateStore}>
+            <LmdbContextProvider>
                 <WebSocketContextProvider url={wsUrl}>
                     <PaperbaseContent/>
                 </WebSocketContextProvider>
+            </LmdbContextProvider>
         </Provider>
     );
 }
