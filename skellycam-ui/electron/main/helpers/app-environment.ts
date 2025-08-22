ea4d5700942a168cc29ce995cdac4cf4aa0aecc2
jonmatthis <jonmatthis@gmail.com>
2025-08-22 19:22:19 -0400

    auto launch python

--- skellycam-ui/electron/main/helpers/app-environment.ts
+++ skellycam-ui/electron/main/helpers/app-environment.ts
@@ -2,6 +2,6 @@
 // Export the ENV_CONFIG object using the process.env values
 export const APP_ENVIRONMENT = {
     IS_DEV: process.env.NODE_ENV === 'development',
-    SHOULD_LAUNCH_PYTHON:  false,
+    SHOULD_LAUNCH_PYTHON:  true,
 
 };
