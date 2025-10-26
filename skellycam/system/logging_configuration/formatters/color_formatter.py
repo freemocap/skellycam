de1a5cf5ae359c62883219d29db988f7cd34f50f
jonmatthis <jonmatthis@gmail.com>
2025-10-26 15:22:23 -0400

    Revert "tweakin"
    This reverts commit 047ccf631dcc965a687cef66359c7f63b8eab7e6.

--- skellycam/system/logging_configuration/formatters/color_formatter.py
+++ skellycam/system/logging_configuration/formatters/color_formatter.py
@@ -20,6 +20,7 @@ class ColorFormatter(CustomFormatter):
 
     def format(self, record: logging.LogRecord) -> str:
         # Apply PID/TID colors
+        record = deepcopy(record)
         record.pid_color = get_hashed_color(record.process)
         record.tid_color = get_hashed_color(record.thread)
 
