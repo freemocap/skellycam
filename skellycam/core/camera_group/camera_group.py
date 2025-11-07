bd9c965fa5341138ec93640c6adb9958d763c63e
jonmatthis <jonmatthis@gmail.com>
2025-11-07 14:12:28 -0500

    add 'started' check

--- skellycam/core/camera_group/camera_group.py
+++ skellycam/core/camera_group/camera_group.py
@@ -41,6 +41,7 @@ class CameraGroup:
     configs: CameraConfigs
     cameras: CameraManager
     shm: CameraGroupSharedMemory | None = None
+    started: bool = False
 
     @property
     def id(self) -> CameraGroupIdString:
@@ -73,6 +74,7 @@ class CameraGroup:
         )
 
     def start(self) -> CameraConfigs:
+        if self.started:
         logger.info(f"Starting camera group ID: {self.id} with cameras: {list(self.configs.keys())}")
         self.cameras.start()
         logger.debug(f"Awaiting extracted configs so we can create shared memory...")
