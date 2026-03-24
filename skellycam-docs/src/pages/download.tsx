8afc5569ab2c02ec8f3c34950855a8207247627c
jonmatthis <jonmatthis@gmail.com>
2026-03-24 14:21:18 -0400

    add human curation and todo notes to the docs

--- skellycam-docs/src/pages/download.tsx
+++ skellycam-docs/src/pages/download.tsx
@@ -1,6 +1,9 @@
 import Layout from '@theme/Layout';
 import DownloadPage from '../components/download/DownloadPage';
 
+// TODO - This page shows up without the header when I go to the link directly, but the header shows up if I click the link to it from the docs home
+
+
 export default function Download() {
   return (
     <Layout title="Download" description="Download SkellyCam">
--- skellycam-docs/src/pages/index.tsx
+++ skellycam-docs/src/pages/index.tsx
@@ -1,6 +1,13 @@
 import { IndexPage } from '@freemocap/skellydocs';
 import config from '../../content.config';
 
+// TODO NOTES: Need to use the new skellydocs Index/config templates to:
+// - change the CTA buttons to go to :
+// - - Get Started (go to 'beginner tutorial'),
+// - - Download/Install (go to download page)
+// - -  Learn More (goes to top level docs page)
+
+// Also the Gurarntees at the bottom needs to show "SkellyCAM is carefully designed to guarantee: ..." Right now it just says 'Skelly...'
 
 export default function Home() {
   return <IndexPage config={config} />;
