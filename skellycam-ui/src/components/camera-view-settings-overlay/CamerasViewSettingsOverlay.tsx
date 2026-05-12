40d7d36c01ebc4971b93b150477fa9cb5ef15945
Pooya Deperson <poamrd@gmail.com>
2026-05-12 10:00:01 +0900

    adding tag styles -

--- skellycam-ui/src/components/camera-view-settings-overlay/CamerasViewSettingsOverlay.tsx
+++ skellycam-ui/src/components/camera-view-settings-overlay/CamerasViewSettingsOverlay.tsx
@@ -119,7 +119,7 @@ export const CamerasViewSettingsOverlay: React.FC<CamerasViewSettingsOverlayProp
               </button>
             </div>
             <div className='configure-camera-action-container text-white text md text-align-left flex flex-row items-center gap-1'>
-                   <p className='text-nowrap flex flex-row gap-1 text-gray'><span>5</span>Connected Cameras</p>
+                   <p className='text-nowrap items-center flex flex-row flex-inline gap-1 text-gray'><span className='tag'>5</span>Connected Cameras</p>
                    
                    <button className="button icon-button"
                         onClick="">
