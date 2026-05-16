b7cd97a7e83a5bbb68929c5a991ccb46c5a81bc2
Pooya Deperson <poamrd@gmail.com>
2026-05-16 10:31:11 +0900

    sidebar z-index isseu

--- skellycam-ui/src/components/recording-info-panel/RecordingInfoPanel.tsx
+++ skellycam-ui/src/components/recording-info-panel/RecordingInfoPanel.tsx
@@ -134,7 +134,7 @@ export const RecordingInfoPanel: React.FC = () => {
         : `${recordingInfo.recordingDirectory}/${recordingName}`;
 
     return (
-    <div className="main-side-actions flex flex-col gap-1">
+    <div className="main-side-actions flex flex-col gap-1 z-3">
         <div className="file-directory-group bg-middark br-2 p-1 flex flex-col gap-1 br-1 p-1 pb-2">
             <p class="text-nowrap text-left bg-md text-darkgray p-1">File directory</p>
                 {/* Path & Settings button */}
