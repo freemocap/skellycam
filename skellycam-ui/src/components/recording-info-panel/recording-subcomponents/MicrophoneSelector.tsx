4ba8ddd719aa0393e65a4025b5ac8c7df66ec488
Pooya Deperson <poamrd@gmail.com>
2026-05-23 09:31:22 +0900

    adding new icons - mic idle and mic active state, changing the connected icon for better glow version

--- skellycam-ui/src/components/recording-info-panel/recording-subcomponents/MicrophoneSelector.tsx
+++ skellycam-ui/src/components/recording-info-panel/recording-subcomponents/MicrophoneSelector.tsx
@@ -59,9 +59,9 @@ export const MicrophoneSelector: React.FC<MicrophoneSelectorProps> = ({
     };
 
     return (
-        <div className="flex flex-col gap-1">
+        <div className="microphone-selector-group flex flex-col gap-1">
             <div className="flex items-center gap-1">
-                <span className={clsx("icon icon-size-20", selectedMicIndex >= 0 ? "connected-icon" : "warning-icon")} />
+                <span className={clsx("icon icon-size-20", selectedMicIndex >= 0 ? "mic-active-icon" : "mic-icon")} />
                 <div className={clsx("flex-1", (disabled || loading) && "disabled")}>
                     <NameDropdownSelector
                         options={options}
