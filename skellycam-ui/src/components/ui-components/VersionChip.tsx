f9f1a11aea9109a8962481455c994a8c5d4bb122
Pooya Deperson <poamrd@gmail.com>
2026-05-26 12:44:38 +0900

    splash screen fixes

--- skellycam-ui/src/components/ui-components/VersionChip.tsx
+++ skellycam-ui/src/components/ui-components/VersionChip.tsx
@@ -53,7 +53,7 @@ export const VersionChip: React.FC<VersionChipProps> = ({ variant = 'full', clas
     if (!version) return null;
 
     return (
-        <div className={`version-button-container pos-abs flex flex-row items-center gap-4${className ? ` ${className}` : ''}`} style={style}>
+        <div className={`version-button-container flex-start pos-abs flex flex-col items-center gap-0 ${className ? ` ${className}` : ''}`} style={style}>
             <ButtonSm
                 text={`v${version}`}
                 onClick={checkForUpdate}
