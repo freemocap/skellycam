c6a71279817e6eced30ac7d85d4be87cca6d786a
philipqueen <queen.philip@gmail.com>
2026-05-28 13:01:56 -0600

    consolidate tooltip position enum

--- skellycam-ui/src/components/ui-components/IconButton.tsx
+++ skellycam-ui/src/components/ui-components/IconButton.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import clsx from "clsx";
+import { TooltipPosition } from "./types";
 
 /**
  * =========================================
@@ -70,11 +71,6 @@ import clsx from "clsx";
  * =========================================
  */
 
-type TooltipPosition =
-  | "pos-top"
-  | "pos-bottom"
-  | "pos-left"
-  | "pos-right";
 
 interface IconButtonProps {
   icon: string;
