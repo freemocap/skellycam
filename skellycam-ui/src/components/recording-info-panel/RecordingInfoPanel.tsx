2d403d4e42f1c1ff0f6f4e9690d751449698b914
Pooya Deperson <poamrd@gmail.com>
2026-05-28 13:01:16 +0900

    adding service not available badge part 1 wip

--- skellycam-ui/src/components/recording-info-panel/RecordingInfoPanel.tsx
+++ skellycam-ui/src/components/recording-info-panel/RecordingInfoPanel.tsx
@@ -39,6 +39,7 @@ export const RecordingInfoPanel: React.FC = () => {
     const [recordingTag, setRecordingTag] = useState<string>("");
     const [micDeviceIndex, setMicDeviceIndex] = useState<number>(-1);
     const [pathModalOpen, setPathModalOpen] = useState<boolean>(false);
+    const [microphoneError, setMicrophoneError] = useState<string | null>(null);
 
     const {isElectron, api} = useElectronIPC();
     const {connectedCameraIds} = useServer();
@@ -137,7 +138,7 @@ export const RecordingInfoPanel: React.FC = () => {
 
     return (
     <div className="main-side-actions flex flex-col gap-1 z-3">
-        <div className="file-directory-group bg-middark br-2 p-1 flex flex-col gap-1 br-1 p-1 pb-2">
+        <div className="pos-rel file-directory-group bg-middark br-2 p-1 flex flex-col gap-1 br-1 p-1 pb-2">
             <p className="text-nowrap text-left bg-md text-darkgray p-1">File directory</p>
                 {/* Path & Settings button */}
                 <ButtonSm
@@ -185,6 +186,13 @@ export const RecordingInfoPanel: React.FC = () => {
                     onCreateSubfolderChange={setCreateSubfolder}
                     onCustomSubfolderNameChange={setCustomSubfolderName}
                 />
+                {microphoneError && (
+                    <div className="error-message-container pos-abs left-50 fit-content bg-warning p-1 flex flex-row text-center items-center">
+                        <p className="error-message text sm text-white text-nowrap overflow-hidden" title={microphoneError}>
+                           {microphoneError}
+                        </p>
+                    </div>
+                )}
             </div>
             {/* Title */}
             {/* <div className="flex items-center gap-1 h-25">
@@ -210,6 +218,7 @@ export const RecordingInfoPanel: React.FC = () => {
                 selectedMicIndex={micDeviceIndex}
                 onMicSelected={setMicDeviceIndex}
                 disabled={recordingInfo.isRecording}
+                onError={setMicrophoneError}
             />
         </div>
         
